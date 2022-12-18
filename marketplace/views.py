c6919268d983d086450b3a662a9d7150b4dfa769
PY-Dev20 <aboodi.nour@gmail.com>
2022-12-18 15:32:33 +0400

    Cart functionalities with AJAX request

--- marketplace/views.py
+++ marketplace/views.py
@@ -2,23 +2,23 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 
 from accounts.models import UserProfile
-#from .context_processors import get_cart_counter, get_cart_amounts
+from .context_processors import get_cart_counter, get_cart_amounts
 from menu.models import Category, FoodItem
 
-from vendor.models import  Vendor
+from vendor.models import OpeningHour, Vendor
 from django.db.models import Prefetch
-#from .models import Cart
+from .models import Cart
 from django.contrib.auth.decorators import login_required
-#from django.db.models import Q
+from django.db.models import Q
 
-#from django.contrib.gis.geos import GEOSGeometry
-#from django.contrib.gis.measure import D # ``D`` is a shortcut for ``Distance``
-#from django.contrib.gis.db.models.functions import Distance
+# from django.contrib.gis.geos import GEOSGeometry
+# from django.contrib.gis.measure import D # ``D`` is a shortcut for ``Distance``
+# from django.contrib.gis.db.models.functions import Distance
 
 from datetime import date, datetime
 #from orders.forms import OrderForm
 
-# Create your views here.
+
 def marketplace(request):
     vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)
     vendor_count = vendors.count()
@@ -27,6 +27,7 @@ def marketplace(request):
         'vendor_count': vendor_count,
     }
     return render(request, 'marketplace/listings.html', context)
+
 
 def vendor_detail(request, vendor_slug):
     vendor = get_object_or_404(Vendor, vendor_slug=vendor_slug)
@@ -38,22 +39,99 @@ def vendor_detail(request, vendor_slug):
         )
     )
 
-    #opening_hours = OpeningHour.objects.filter(vendor=vendor).order_by('day', 'from_hour')
+    opening_hours = OpeningHour.objects.filter(vendor=vendor).order_by('day', 'from_hour')
     
     # Check current day's opening hours.
     today_date = date.today()
     today = today_date.isoweekday()
     
-    # current_opening_hours = OpeningHour.objects.filter(vendor=vendor, day=today)
-    # if request.user.is_authenticated:
-    #     cart_items = Cart.objects.filter(user=request.user)
-    # else:
-    #     cart_items = None
+    current_opening_hours = OpeningHour.objects.filter(vendor=vendor, day=today)
+    if request.user.is_authenticated:
+        cart_items = Cart.objects.filter(user=request.user)
+    else:
+        cart_items = None
     context = {
         'vendor': vendor,
         'categories': categories,
-       # 'cart_items': cart_items,
-        #'opening_hours': opening_hours,
-        #'current_opening_hours': current_opening_hours,
+        'cart_items': cart_items,
+        'opening_hours': opening_hours,
+        'current_opening_hours': current_opening_hours,
     }
     return render(request, 'marketplace/vendor_detail.html', context)
+
+
+def add_to_cart(request, food_id):
+    if request.user.is_authenticated:
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            # Check if the food item exists
+            try:
+                fooditem = FoodItem.objects.get(id=food_id)
+                # Check if the user has already added that food to the cart
+                try:
+                    chkCart = Cart.objects.get(user=request.user, fooditem=fooditem)
+                    # Increase the cart quantity
+                    chkCart.quantity += 1
+                    chkCart.save()
+                    return JsonResponse({'status': 'Success', 'message': 'Increased the cart quantity', 'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity, 'cart_amount': get_cart_amounts(request)})
+                except:
+                    chkCart = Cart.objects.create(user=request.user, fooditem=fooditem, quantity=1)
+                    return JsonResponse({'status': 'Success', 'message': 'Added the food to the cart', 'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity, 'cart_amount': get_cart_amounts(request)})
+            except:
+                return JsonResponse({'status': 'Failed', 'message': 'This food does not exist!'})
+        else:
+            return JsonResponse({'status': 'Failed', 'message': 'Invalid request!'})
+        
+    else:
+        return JsonResponse({'status': 'login_required', 'message': 'Please login to continue'})
+
+
+def decrease_cart(request, food_id):
+    if request.user.is_authenticated:
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            # Check if the food item exists
+            try:
+                fooditem = FoodItem.objects.get(id=food_id)
+                # Check if the user has already added that food to the cart
+                try:
+                    chkCart = Cart.objects.get(user=request.user, fooditem=fooditem)
+                    if chkCart.quantity > 1:
+                        # decrease the cart quantity
+                        chkCart.quantity -= 1
+                        chkCart.save()
+                    else:
+                        chkCart.delete()
+                        chkCart.quantity = 0
+                    return JsonResponse({'status': 'Success', 'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity, 'cart_amount': get_cart_amounts(request)})
+                except:
+                    return JsonResponse({'status': 'Failed', 'message': 'You do not have this item in your cart!'})
+            except:
+                return JsonResponse({'status': 'Failed', 'message': 'This food does not exist!'})
+        else:
+            return JsonResponse({'status': 'Failed', 'message': 'Invalid request!'})
+        
+    else:
+        return JsonResponse({'status': 'login_required', 'message': 'Please login to continue'})
+
+
+@login_required(login_url = 'login')
+def cart(request):
+    cart_items = Cart.objects.filter(user=request.user).order_by('created_at')
+    context = {
+        'cart_items': cart_items,
+    }
+    return render(request, 'marketplace/cart.html', context)
+
+
+def delete_cart(request, cart_id):
+    if request.user.is_authenticated:
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            try:
+                # Check if the cart item exists
+                cart_item = Cart.objects.get(user=request.user, id=cart_id)
+                if cart_item:
+                    cart_item.delete()
+                    return JsonResponse({'status': 'Success', 'message': 'Cart item has been deleted!', 'cart_counter': get_cart_counter(request), 'cart_amount': get_cart_amounts(request)})
+            except:
+                return JsonResponse({'status': 'Failed', 'message': 'Cart Item does not exist!'})
+        else:
+            return JsonResponse({'status': 'Failed', 'message': 'Invalid request!'})
--- vendor/views.py
+++ vendor/views.py
@@ -5,13 +5,13 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.db import IntegrityError
 
 from menu.forms import CategoryForm, FoodItemForm
-# from orders.models import Order, OrderedFood
+#from orders.models import Order, OrderedFood
 import vendor
-from .forms import VendorForm
+from .forms import VendorForm, OpeningHourForm
 from accounts.forms import UserProfileForm
 
 from accounts.models import UserProfile
-from .models import Vendor
+from .models import OpeningHour, Vendor
 from django.contrib import messages
 
 from django.contrib.auth.decorators import login_required, user_passes_test
@@ -19,10 +19,11 @@ from accounts.views import check_role_vendor
 from menu.models import Category, FoodItem
 from django.template.defaultfilters import slugify
 
-# Create your views here.
+
 def get_vendor(request):
     vendor = Vendor.objects.get(user=request.user)
     return vendor
+
 
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
@@ -53,6 +54,7 @@ def vprofile(request):
     }
     return render(request, 'vendor/vprofile.html', context)
 
+
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
 def menu_builder(request):
@@ -62,6 +64,7 @@ def menu_builder(request):
         'categories': categories,
     }
     return render(request, 'vendor/menu_builder.html', context)
+
 
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
@@ -74,6 +77,7 @@ def fooditems_by_category(request, pk=None):
         'category': category,
     }
     return render(request, 'vendor/fooditems_by_category.html', context)
+
 
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
@@ -196,3 +200,73 @@ def delete_food(request, pk=None):
     food.delete()
     messages.success(request, 'Food Item has been deleted successfully!')
     return redirect('fooditems_by_category', food.category.id)
+
+
+def opening_hours(request):
+    opening_hours = OpeningHour.objects.filter(vendor=get_vendor(request))
+    form = OpeningHourForm()
+    context = {
+        'form': form,
+        'opening_hours': opening_hours,
+    }
+    return render(request, 'vendor/opening_hours.html', context)
+
+
+def add_opening_hours(request):
+    # handle the data and save them inside the database
+    if request.user.is_authenticated:
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest' and request.method == 'POST':
+            day = request.POST.get('day')
+            from_hour = request.POST.get('from_hour')
+            to_hour = request.POST.get('to_hour')
+            is_closed = request.POST.get('is_closed')
+            
+            try:
+                hour = OpeningHour.objects.create(vendor=get_vendor(request), day=day, from_hour=from_hour, to_hour=to_hour, is_closed=is_closed)
+                if hour:
+                    day = OpeningHour.objects.get(id=hour.id)
+                    if day.is_closed:
+                        response = {'status': 'success', 'id': hour.id, 'day': day.get_day_display(), 'is_closed': 'Closed'}
+                    else:
+                        response = {'status': 'success', 'id': hour.id, 'day': day.get_day_display(), 'from_hour': hour.from_hour, 'to_hour': hour.to_hour}
+                return JsonResponse(response)
+            except IntegrityError as e:
+                response = {'status': 'failed', 'message': from_hour+'-'+to_hour+' already exists for this day!'}
+                return JsonResponse(response)
+        else:
+            HttpResponse('Invalid request')
+
+
+def remove_opening_hours(request, pk=None):
+    if request.user.is_authenticated:
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            hour = get_object_or_404(OpeningHour, pk=pk)
+            hour.delete()
+            return JsonResponse({'status': 'success', 'id': pk})
+
+
+# def order_detail(request, order_number):
+#     try:
+#         order = Order.objects.get(order_number=order_number, is_ordered=True)
+#         ordered_food = OrderedFood.objects.filter(order=order, fooditem__vendor=get_vendor(request))
+
+#         context = {
+#             'order': order,
+#             'ordered_food': ordered_food,
+#             'subtotal': order.get_total_by_vendor()['subtotal'],
+#             'tax_data': order.get_total_by_vendor()['tax_dict'],
+#             'grand_total': order.get_total_by_vendor()['grand_total'],
+#         }
+#     except:
+#         return redirect('vendor')
+#     return render(request, 'vendor/order_detail.html', context)
+
+
+# def my_orders(request):
+#     vendor = Vendor.objects.get(user=request.user)
+#     orders = Order.objects.filter(vendors__in=[vendor.id], is_ordered=True).order_by('created_at')
+
+#     context = {
+#         'orders': orders,
+#     }
+#     return render(request, 'vendor/my_orders.html', context)
